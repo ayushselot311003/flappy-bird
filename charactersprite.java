b5287c2e4fc059d5356d6ed4b34f6d31202b239d
ayushselot311003 <100141728+ayushselot311003@users.noreply.github.com>
2022-02-21 19:47:39 +0530

    Add files via upload

---
+++ charactersprite.java
@@ -0,0 +1,43 @@
+package com.example.flappybird;
+
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.hardware.SensorManager;
+
+
+
+
+ class CharacterSprite{
+
+
+    private Bitmap image;
+    public int x, y;
+    private int xVelocity = 10;
+    public int yVelocity = 5;
+    private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+
+
+    public CharacterSprite(Bitmap bmp) {
+        image = bmp;
+        x = 100;
+        y = 100;
+    }
+
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(image, x, y, null);
+
+
+    }
+
+    public void update() {
+
+        y += yVelocity;
+
+
+    }
+}
